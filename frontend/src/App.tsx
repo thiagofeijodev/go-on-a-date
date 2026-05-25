d5b6541ea2891ac39d7f911e4954f2ea7d0ceacb
thiagofeijor <thiago@feijo.dev>
2026-05-25 18:50:34 +0100

    fix: url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Invite from './pages/Invite';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/invite/:token" element={<Invite />} />
